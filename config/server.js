3216c2e423803e26f52dafd8e3025c89c9f65cbd
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-01 18:28:42 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '75d3dfa554a6b271b833573013db3053'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c4b71af1-1d60-4699-9ae7-8846ba2099dd'
+};
